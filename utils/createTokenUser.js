56eb3b1851eaf527ac331d0a0be6893b231cf638
tomiwadotJS <victorgbayedev@gmail.com>
2023-12-07 01:15:24 +0000

    changes

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,5 +1,5 @@
 const createTokenUser = (user) => {
-  return { firstname: user.firstname,lastname: user.lastname, userId: user._id, role: user.role, subscription: user.subscription };
+  return { firstname: user.firstname,lastname: user.lastname, email: user.email, userId: user._id, role: user.role, subscription: user.subscription };
 };
 
 module.exports = createTokenUser;
