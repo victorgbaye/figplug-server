5d954a24113806267e334c5b23312b2e780a980c
tomiwadotJS <victorgbayedev@gmail.com>
2023-12-16 12:34:48 +0000

    changes

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -2,7 +2,7 @@ const User  = require('../models/User')
 // const customError = require('../errors')
 const stripe = require('stripe')('sk_test_51MxbsUEEwSz12DzqASH4j5lUy2Aq8YuB15y33HvjN1py7g9A9sx7QtL4II3XewmCuk5IIQ91xnzjUkJCRU2u4qLy00t2bd2NJ4');
 
-const YOUR_DOMAIN = 'http://127.0.0.1:5173';
+const YOUR_DOMAIN = 'https://thefigplug.onrender.com/';
 
 const createCheckoutSession = async (req, res) => {
   try {
